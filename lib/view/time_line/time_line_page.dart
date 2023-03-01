107623f4babd7834801408954a7d93326cf32245
seitamuro <seitamuro0210@gmail.com>
2023-03-01 21:05:36 +0900

    タイムラインを開発するためのアカウントを作成

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../model/account.dart';
+
 class TimeLinePage extends StatefulWidget {
   @override
   State<TimeLinePage> createState() => _TimeLinePageState();
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
+  Account myAccount = Account(
+    id: "1",
+    name: "seitamuro",
+    userId: "seitamuro",
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
